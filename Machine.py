9fcccc808baa98612d3232921af998ae6b66fdb8
cubedGuy <47624167+cubedGuy@users.noreply.github.com>
2019-02-15 17:59:19 +0530

    Updated Virtual Machine
    It runs now.
    But doesn't do anything as such yet.

--- Machine.py
+++ Machine.py
@@ -1,17 +1,12 @@
-with open("Test.hx", "rb") as f: file = f.read()
-print(file)
-size = len(file)
-mem = ""
-ws = [9, 10, 13, 32]
-i = 0
-print("Reading...")
-while i < size:
-	while file[i] in ws: i += 1	
-	if file[i] in [48, 49]:
-		mem += chr(file[i])
-		i += 1
-	elif file[i] == 35:
-		while file[i] != 10: i += 1
-	else:
-		print("Invalid character:", file[i])
-		quit()
+import pygame
+
+with open("File.lx", "rb") as f: mem = f.read()
+
+pygame.init()
+
+display = pygame.display.set_mode((16, len(mem)))
+
+running = True
+
+while running:
+	running = not(pygame.QUIT in [event.type for event in pygame.event.get()])
