5cc617cfc11daaf89cf12f68da043e20d76e7dc7
cubedGuy <47624167+cubedGuy@users.noreply.github.com>
2019-02-15 14:27:50 +0530

    Adding names feature
    It doesn't run yet.
    Still working on it.

--- Compiler.py
+++ Compiler.py
@@ -2,23 +2,52 @@ with open("File.hx", "rb") as f: file = f.read()
 
 size = len(file)
 ws = b"\t\n\r "
-i = 0
-mem = ""
-names = {}
+letters = [a for a in range(65, 91) + range(97, 123)]
 
+i = 0
+line = 1
+
+names = {'mem':""}
+hier = ['mem']						#path of hierarchy
+
+def noRead():						#for skipping comments and white space
+	global i, file, ws, size, line
+	# stay = file[i] in ws or file[i:(i+2)] == b'//'
+	# while stay:
+		# i += 1
+		# if i >= size: break
+		# if file[i:(i+2)] == b'//':		#for commenting 
+			# i += 2
+			# while file[i] != 13:		# ord(13) -> carriage return character
+				# i += 1
+				# line += 1
+				# if i >= size: break
+	space = file[i] in ws
+	comment = file[i:(i+2)] == b'//'
+	while space or comment:
+		i += 1
+		if i >= size: break
+		
+		
 while i < size:
 	if file[i] in b'01':   				#adding bits
-		mem += chr(file[i])
+		names[name] += chr(file[i])
 		i += 1
-	elif file[i:(i+2)] == b'//':		#for commenting 
-		while file[i] != 13: i += 1		# ord(13) -> '\r'
+	elif file[i] == 36:					# ord(36) -> '$'
+		name = ""
+		i += 1
+		while file[i] in letters:
+			name += file[i]
+			i += 1
+		if name in names:
+			print("Name", name, "already exists.")
+		names[name] = ""
+		noRead()
 	else:
 		print("Invalid character:", chr(file[i]))
 		print(file[i:(i+1)])
 		quit()
-	while file[i] in ws:
-		i += 1
-		if i >= size: break
+	noRead()
 
 mSize = len(mem)
 
