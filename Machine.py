b727da0402982679c70271c05c81e86da9a81064
cubedGuy <47624167+cubedGuy@users.noreply.github.com>
2019-05-11 12:07:32 +0530

    New Machine

--- Machine.py
+++ Machine.py
@@ -1,12 +1,29 @@
-import pygame
+mem = [1] + [255]*255
 
-with open("File.lx", "rb") as f: mem = f.read()
+print("Reading...")
 
-pygame.init()
+with open("File.lx", 'rb') as file:
+	i = 1
+	while 1:
+		char = file.read(1)
+		if char == b'': break
+		mem[i] = ord(char)
 
-display = pygame.display.set_mode((16, len(mem)))
+op = []
+for o in mem[196:]: op += [bool(a) for a in o]
 
-running = True
+print("Running...")
 
-while running:
-	running = not(pygame.QUIT in [event.type for event in pygame.event.get()])
+while 1:
+	fetch = mem[0]
+	destination, source = mem[fetch], mem[fetch+1]
+	mem[0] = fetch + 2
+	data = mem[destination] = mem[source]
+	if destination >= 196:
+		loc = (destination - 196)*8
+		data = [bool(int(a)) for a in str(data, 2)]
+		for a in range(8): op[loc+a] = data[a]
+	for main in range(16):
+		val = False
+
+	cycles += 1
