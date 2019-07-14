412ba5a0c373c9176166ecae9b6e709ba75243e5
cubedGuy <47624167+cubedGuy@users.noreply.github.com>
2019-07-14 22:39:41 +0530

    Added OP support in mem
    OP will be added soon

--- Machine.py
+++ Machine.py
@@ -1,27 +1,51 @@
-Ghz = 3.0 #Billion cycles per second
+GHz = 4.0 #Billion cycles per second
+Loops = 5 #Loops OP is allowed to make per cycle
 
-with open("File.lx", 'rb') as file:
+with open("File.lx", 'rb') as file:	#read file
 	def give(string):
 		if len(string): return ord(string)
 		else: return 0
 	mem = [1]+list(map(lambda x: give(file.read(1)), [None]*255))
+	out = [0, 0, 0]
 	del give
 
 cycles = 0
-fetch = 1
-print(f"{fetch}: {mem[mem[fetch]]} from {mem[fetch+1]} to {mem[fetch]}")
+fetch = 0
 print(mem)
-while mem[0] != mem[mem[fetch]]:
-	if mem[0]>=255: break
+
+while mem[0] != fetch:
 	fetch = mem[0]
 	mem[0] += 2
-	mem[mem[fetch]] = mem[mem[fetch+1]]
-	print(f"{fetch}: {mem[mem[fetch]]} from {mem[fetch+1]} to {mem[fetch]}")
 
-	
+	#getting data to be transferred
+	if mem[fetch+1] in range(37, 40):
+		Data = out[mem[fetch+1]-37]
+	else: Data = mem[mem[fetch+1]]
+
+	mem[mem[fetch]] = Data
+	print(f"{fetch}: {Data} from {mem[fetch+1]} to {mem[fetch]}")
+	print(f"OP output:{', '.join(list(map(lambda x: bin(x)[2:], out)))}")
+	#
+	#"".join(list(map(lambda x: bin(x)[2:], out)))
+	#"".join([bin1, bin2, bin3])
+	#bin1+bin2+bin3 -> str
+
+	#OP starts from mem[37]
+	#	mem[37:40]   -> IO
+	#	mem[40:112]  -> OP inputs Grid
+	#	mem[112:184] -> OP main Grid
+	# 	mem[184:256] -> OP inverted Grid
+
+	for i in range(Loops):
+
+		
 
 	cycles += 1
+	if mem[0]>=255: break
+	elif mem[0] == 0:
+		print("FetchError: Fetch cannot be zero.")
+		break
 else: print("InfiniteLoopError: Fetch assigning to itself.")
 print(f"Process completed in {cycles} cycles.\
-	({cycles/Ghz} nanoseconds on {Ghz}Ghz)")
+	({cycles/GHz} nanoseconds on {GHz}GHz)")
 print(mem)
