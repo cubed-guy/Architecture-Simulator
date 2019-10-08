68344347792b4ae8c2af85bf241022a91e02a7f7
cubedGuy <47624167+cubedGuy@users.noreply.github.com>
2019-10-08 21:54:16 +0530

    Complex hx file
    Before compiling.
    (tests every aspect I believe)
    Output is expected to be 64kB.

--- File.hx
+++ File.hx
@@ -1,6 +1,19 @@
-00 05		// START: jump to REG06. REG05 = 6
-
-c0 ff 06 	// store c0, ff, 06
-
-c1 03 		// move value at REG03 (c0) to REGc1
-00 04 		// END: jump to REGff. REG04 = ff
+#defs...
+0{00}
+#Easy?
+1{0 01 0}
+2{1 02 1}
+3{2 03 2}	#not too interesting here
+4{3{04}3}
+5{4 05 4}
+6{5 06 5}
+7{6 07 6}
+8{7 08 7}
+9{8 09 8}
+a{9 0a 9}
+b{a 0b a}
+c{b 0c b}
+#Mess?
+e{{c 0d c} 0e {c 0d c}}
+#DATA!!!
+e {0f e}
